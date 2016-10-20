e43cfa16f8c3295f8796bd7efc3f387d07422f67
Samuel <samheilbron@Samuels-MacBook-Pro-4.local>
2016-10-20 17:21:21 -0400

    Start problem 2

---
+++ 2.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+##
+## Use memoization to implement dynamic programming
+##
+
+
+import sys
+import collections
+
+
+def main(argv):
+
+	return
+
+
+
+if __name__ == '__main__':
+    main(sys.argv)
