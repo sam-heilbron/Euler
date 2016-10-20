a59c31bd3f6927bab3f1020ce0671f8c5430ec7f
Samuel <samheilbron@Samuels-MacBook-Pro-4.local>
2016-10-20 17:14:30 -0400

    Finished Problem 1

--- 1.py
+++ 1.py
@@ -5,6 +5,25 @@
 ##
 ## Sum(0 --> n) i = n(n+1)/2
 
-def sum(n):
-	
+import sys
 
+def main(argv):
+	maxNum = 1000
+	
+	sum3  = 3  * geoSum(multiplesUnderN( 3, maxNum))
+	sum5  = 5  * geoSum(multiplesUnderN( 5, maxNum))
+	sum15 = 15 * geoSum(multiplesUnderN(15, maxNum))
+
+	print (sum3 + sum5 - sum15)
+	return
+
+
+def geoSum(n):
+	return (n * (n+1))/2
+
+def multiplesUnderN(mult, max):
+	return int((max-1) / mult)
+
+
+if __name__ == '__main__':
+    main(sys.argv)
